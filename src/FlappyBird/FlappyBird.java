cd79c0666f55d20535ff1c11e2abc289db32de9d
Tom <tomberey1@gmail.com>
2020-05-01 14:23:32 +0100

    Initial commit

--- src/FlappyBird/FlappyBird.java
+++ src/FlappyBird/FlappyBird.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.JFrame;
 import javax.swing.Timer;
+import javax.swing.WindowConstants;
 
 public class FlappyBird implements ActionListener, MouseListener, KeyListener {
 
@@ -40,7 +41,7 @@ public class FlappyBird implements ActionListener, MouseListener, KeyListener {
         rand = new Random();
 
         jframe.add(renderer);
-        jframe.setDefaultCloseOperation(jframe.EXIT_ON_CLOSE);
+        jframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         jframe.setSize(WIDTH, HEIGHT);
         jframe.addMouseListener(this);
         jframe.addKeyListener(this);
